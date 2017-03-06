6f53e952093f853c1510e9c7939ff4a66ed62ad7
dondbui <dondbui@gmail.com>
2017-03-05 20:00:24 -0800

    Create TileMapExperiment project
    - Created the TileMapExperiment project
    - Imported and setup the first sample tile asset in SpriteEditor

---
+++ TileMapExperiment/Assets/Scripts/Core/MainLoop.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * Mainloop.cs
+ * 
+ * <author>Don Duy Bui</author>
+ * <date>March 5th, 2017</date>
+ */
+using UnityEngine;
+
+/// <summary>
+/// This class handles the main game loop of the system. 
+/// </summary>
+public class MainLoop : MonoBehaviour {
+
+    // Use this for initialization
+    void Start ()
+    {
+        Debug.Log("Game Initializing");
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+}
---
+++ TileMapExperiment/README.md
@@ -0,0 +1,8 @@
+# TileMapExperiment
+This project is for my own personal experimentation with tilemap systems using Unity2D
+
+##### Sources:
+
+###### Tile Art: 
+I take no credit for the art taken from the following open sources.
+- http://opengameart.org/content/tileset32x32-grass-seasons-rocks-gravel
