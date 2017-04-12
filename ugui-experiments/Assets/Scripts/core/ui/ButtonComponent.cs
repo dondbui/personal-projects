d347a68a4ed778036f497546336d39a80cd856f0
dondbui <dondbui@gmail.com>
2017-04-12 00:26:46 -0700

    Add in basic button click handling for the title screen

---
+++ ugui-experiments/Assets/Scripts/core/ui/ButtonComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace core.ui
+{
+    /// <summary>
+    /// This component is there to forward button behavior over to
+    /// the UIInputController.
+    /// </summary>
+    public class ButtonComponent : MonoBehaviour
+    {
+        private Button button;
+
+
+        void Start()
+        {
+            button = this.gameObject.GetComponent<Button>();
+        }
+
+        public void OnClicked()
+        {
+            UIInputController.GetInstance().ButtonClicked(button);
+        }
+    }
+}
---
+++ ugui-experiments/Assets/Scripts/core/ui/UIInputController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace core.ui
+{
+    /// <summary>
+    /// This acts as the central Observer for all of the button
+    /// clicks for the UI
+    /// </summary>
+    public class UIInputController
+    {
+        private static UIInputController instance;
+
+        private UIInputController()
+        {
+
+        }
+
+        public static UIInputController GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new UIInputController();
+            }
+
+            return instance;
+        }
+
+        public void ButtonClicked(Button button)
+        {
+            Debug.Log("ButtonClicked: " + button.name);
+
+
+        }
+        
+    }
+}
