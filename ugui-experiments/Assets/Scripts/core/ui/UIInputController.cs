c2c578f6a87c26b57875393129d7b0915503611f
dondbui <dondbui@gmail.com>
2017-05-23 00:06:04 -0700

    Add a way to register button listeners by routing all button click through the UIInputController

--- ugui-experiments/Assets/Scripts/core/ui/UIInputController.cs
+++ ugui-experiments/Assets/Scripts/core/ui/UIInputController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace core.ui
@@ -9,11 +10,15 @@ namespace core.ui
     /// </summary>
     public class UIInputController
     {
+        public delegate void OnClick(Button button);
+        
         private static UIInputController instance;
+
+        private Dictionary<Button, List<OnClick>> callbackMap;
 
         private UIInputController()
         {
-
+            callbackMap = new Dictionary<Button, List<OnClick>>();
         }
 
         public static UIInputController GetInstance()
@@ -26,11 +31,61 @@ namespace core.ui
             return instance;
         }
 
+        public void RegisterOnClick(Button button, OnClick callback)
+        {
+            if (!callbackMap.ContainsKey(button))
+            {
+                callbackMap[button] = new List<OnClick>();
+            }
+
+            List<OnClick> callbackList = callbackMap[button];
+
+            // Already been registered don't care to re-register and have double calls
+            if (callbackList.Contains(callback))
+            {
+                Debug.LogError("Button Listener Already registered!!");
+                return;
+            }
+
+            callbackList.Add(callback);
+        }
+
+        public void UnregisterOnClick(Button button, OnClick callback)
+        {
+            // Can't unregister something that hasn't been registered
+            if (!callbackMap.ContainsKey(button))
+            {
+                Debug.LogError("Button Key: " + button.name + " never registered!!");
+                return;
+            }
+
+            List<OnClick> callbackList = callbackMap[button];
+
+            if (!callbackList.Contains(callback))
+            {
+                Debug.LogError("Button: " + button.name + " never registered!!");
+                return;
+            }
+
+            callbackList.Remove(callback);
+        }
+
         public void ButtonClicked(Button button)
         {
             Debug.Log("ButtonClicked: " + button.name);
 
+            // Doesn't exist then bounce out
+            if (!callbackMap.ContainsKey(button))
+            {
+                return;
+            }
 
+            // Iterate through all the listeners and call them
+            List<OnClick> callbackList = callbackMap[button];
+            for (int i = 0, count = callbackList.Count; i < count; i++)
+            {
+                callbackList[i](button);
+            }
         }
         
     }
---
+++ ugui-experiments/Assets/Scripts/core/ui/screens/TitleScreen.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace core.ui.screens
+{
+    public class TitleScreen : MonoBehaviour
+    {
+        private const string BTN_NEWGAME = "NewGame";
+        private const string BTN_CONTINUE = "Continue";
+
+        private Button newGameBtn;
+        private Button continueBtn;
+
+        public void Start()
+        {
+            newGameBtn = GetButtonByName(transform, BTN_NEWGAME);
+            continueBtn = GetButtonByName(transform, BTN_CONTINUE);
+
+            UIInputController uic = UIInputController.GetInstance();
+
+            uic.RegisterOnClick(newGameBtn, OnNewGameClicked);
+            uic.RegisterOnClick(continueBtn, OnContinueClicked);
+        }
+
+        private void OnNewGameClicked(Button button)
+        {
+            Debug.Log("Handle New Game Button");
+        }
+
+        private void OnContinueClicked(Button button)
+        {
+            Debug.Log("Handle Continue Button");
+        }
+
+        private Button GetButtonByName(Transform transform, string name)
+        {
+            // Try to the find the child transform by name
+            Transform btnTransform = transform.Find(name);
+
+            // If we couldn't find it then it'll be null
+            if (btnTransform == null)
+            {
+                return null;
+            }
+
+            // Now we can safely get the gameobject off of the found tranform
+            GameObject btnObj = btnTransform.gameObject;
+
+            // Return the button component
+            return btnObj.GetComponent<Button>();
+        }
+    }
+}
